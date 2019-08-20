2dd8419da543de36993c2c581d8c53f9b9c58901
SAKSHI11062001SAWANT <54312378+SAKSHI11062001SAWANT@users.noreply.github.com>
2019-08-20 16:29:21 +0530

    Add files via upload

---
+++ Assignment operator.py
@@ -0,0 +1,31 @@
+Python 3.4.3 (v3.4.3:9b73f1c3e601, Feb 24 2015, 22:43:06) [MSC v.1600 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> x=8
+>>> y=x+5
+>>> print(y)
+13
+>>> 
+>>> x=8-5
+>>> print(x)
+3
+>>> 
+>>> x=5*3
+>>> print(x)
+15
+>>> 
+>>> x=5/3
+>>> print(x)
+1.6666666666666667
+>>> 
+>>> x=5%3
+>>> print(x)
+2
+>>> 
+>>> x=5//3
+>>> print(x)
+1
+>>> 
+>>> x=5**3
+>>> print(x)
+125
+>>> 
